7d04bbba01e6374f00ffcd60c673ef10d8394e25
Stephen Walsh <sfwalshappdev@gmail.com>
2018-10-28 13:18:02 +0100

    updated launch model

--- EspacioX/Models/Launch.swift
+++ EspacioX/Models/Launch.swift
@@ -2,5 +2,106 @@
 import Foundation
 
 struct Launch: Decodable {
-    
+    /*
+     {
+     "flight_number": 69,
+     "mission_name": "SAOCOM 1A",
+     "mission_id": [],
+     "launch_year": "2018",
+     "launch_date_unix": 1538870400,
+     "launch_date_utc": "2018-10-07T00:00:00.000Z",
+     "launch_date_local": "2018-10-06T17:00:00-07:00",
+     "is_tentative": true,
+     "tentative_max_precision": "day",
+     "rocket": {
+     "rocket_id": "falcon9",
+     "rocket_name": "Falcon 9",
+     "rocket_type": "FT",
+     "first_stage": {
+     "cores": [
+     {
+     "core_serial": "B1048",
+     "flight": 2,
+     "block": 5,
+     "reused": true,
+     "land_success": null,
+     "landing_intent": true,
+     "landing_type": "RTLS",
+     "landing_vehicle": "LZ-3"
+     }
+     ]
+     },
+     "second_stage": {
+     "block": null,
+     "payloads": [
+     {
+     "payload_id": "SAOCOM 1A",
+     "norad_id": [],
+     "reused": false,
+     "customers": [
+     "CONAE"
+     ],
+     "nationality": null,
+     "manufacturer": null,
+     "payload_type": "Satellite",
+     "payload_mass_kg": 1600,
+     "payload_mass_lbs": 3527.4,
+     "orbit": "SSO",
+     "orbit_params": {
+     "reference_system": "geocentric",
+     "regime": "sun-synchronous",
+     "longitude": null,
+     "semi_major_axis_km": null,
+     "eccentricity": null,
+     "periapsis_km": null,
+     "apoapsis_km": null,
+     "inclination_deg": null,
+     "period_min": null,
+     "lifespan_years": null,
+     "epoch": null,
+     "mean_motion": null,
+     "raan": null,
+     "arg_of_pericenter": null,
+     "mean_anomaly": null
+     }
+     }
+     ]
+     },
+     "fairings": {
+     "reused": false,
+     "recovery_attempt": null,
+     "recovered": null,
+     "ship": null
+     }
+     },
+     "ships": [],
+     "telemetry": {
+     "flight_club": null
+     },
+     "launch_site": {
+     "site_id": "vafb_slc_4e",
+     "site_name": "VAFB SLC 4E",
+     "site_name_long": "Vandenberg Air Force Base Space Launch Complex 4E"
+     },
+     "launch_success": null,
+     "links": {
+     "mission_patch": null,
+     "mission_patch_small": null,
+     "reddit_campaign": "https://www.reddit.com/r/spacex/comments/9fwj9o/saocom_1a_launch_campaign_thread/",
+     "reddit_launch": null,
+     "reddit_recovery": null,
+     "reddit_media": null,
+     "presskit": null,
+     "article_link": null,
+     "wikipedia": null,
+     "video_link": null,
+     "flickr_images": []
+     },
+     "details": null,
+     "upcoming": true,
+     "static_fire_date_utc": null,
+     "static_fire_date_unix": null
+     }
+     
+     */
 }
