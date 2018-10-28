02fbc5d2340ae5b0e1ee68fb6c150e90a7c874e6
Stephen Walsh <sfwalshappdev@gmail.com>
2018-10-28 14:57:53 +0100

    implemented upcoming launch request

--- EspacioX/Views/LaunchList/Implementations/Default/LaunchListDefaultInteractor.swift
+++ EspacioX/Views/LaunchList/Implementations/Default/LaunchListDefaultInteractor.swift
@@ -24,11 +24,33 @@ final class LaunchListDefaultInteractor: LaunchListInteractor {
         }.resume()
     }
     
-    func fetchNextLaunch(completion: ((NetworkResult<Launch>) -> Void)) {
+    func fetchNextLaunch(completion: @escaping ((NetworkResult<Launch>) -> Void)) {
+        guard let request = API.buildNextLaunchRequest() else {
+            completion(.failure(error: LaunchFetchError.requestError))
+            return
+        }
         
+        URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
+            if let error = error {
+                completion(.failure(error: LaunchFetchError.network(description: error.localizedDescription)))
+                return
+            }
+            
+            guard let data = data,
+                let launches = self?.parseUpcomingLaunchResponseData(data: data) else {
+                    completion(.failure(error: LaunchFetchError.jsonDecoding))
+                    return
+            }
+            
+            completion(.success(value: launches))
+            }.resume()
     }
     
     private func parseLaunchListResponseData(data: Data) -> [Launch]? {
         return try? JSONDecoder().decode([Launch].self, from: data)
+    }
+    
+    private func parseUpcomingLaunchResponseData(data: Data) -> Launch? {
+        return try? JSONDecoder().decode(Launch.self, from: data)
     }
 }
